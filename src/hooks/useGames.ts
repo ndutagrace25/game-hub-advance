db20b5524ff3d95ec37c5a4b9b56203dec3edbab
ndutagrace25 <ndutagrace25@gmail.com>
2023-11-16 09:15:49 +0300

    Implementing Infinite Scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { FetchResponse } from "../services/api-client";
 import { CATCHE_KEY_GAMES } from "../services/constants";
@@ -15,18 +15,22 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) => {
-  return useQuery({
+  return useInfiniteQuery({
     queryKey: [...CATCHE_KEY_GAMES, gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       gameService.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
+          page: pageParam,
         },
       }),
-    staleTime: 1 * 60 * 60 * 1000, // 1 hr
+    staleTime: 1 * 60 * 60 * 1000, // 1 hr,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ?  allPages.length + 1 : undefined;
+    },
   });
 };
 
