2f5e5a921fecc5a580bae4f548d65af4f5a77ae6
ndutagrace25 <ndutagrace25@gmail.com>
2023-11-16 09:27:55 +0300

    Implement Infinite Scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -27,7 +27,7 @@ const useGames = (gameQuery: GameQuery) => {
           page: pageParam,
         },
       }),
-    staleTime: 1 * 60 * 60 * 1000, // 1 hr,
+    staleTime: 24 * 60 * 60 * 1000, // 24 hr,
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ?  allPages.length + 1 : undefined;
     },
